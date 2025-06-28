b13001d6e7ebdf834a8e9d4aa124b5d88b01db1c
patelrudresh <rudreshpatel728@gmail.com>
2025-06-28 09:27:18 +0530

    Added API response DTO, serviceImpl, and updated Employee-service components

--- Employee-service/src/main/java/com/MVC/Employee_service/entity/Employee.java
+++ Employee-service/src/main/java/com/MVC/Employee_service/entity/Employee.java
@@ -1,3 +1,4 @@
+
 package com.MVC.Employee_service.entity;
 
 import jakarta.persistence.Column;
@@ -11,22 +12,27 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "EMPLOYE")  // Use your exact Oracle table name
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
-@Entity
-@Table(name="employeService")
 public class Employee {
-	
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private Long id;
-		@Column(nullable =false )
-		private String firstName;
-		@Column
-		private String lastName;
-		@Column(unique = true,nullable = false)
-		private String employeEmail;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "EMPLOYE_EMAIL", unique = true, nullable = false)
+    private String employeEmail;
+
+    @Column(name = "DEPT_CODE")
+    private String deptCode;
 }
