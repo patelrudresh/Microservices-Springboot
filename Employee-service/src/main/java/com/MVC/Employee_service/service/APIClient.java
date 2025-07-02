ddd55555ccb38a36d837f16279a50363d44c90fe
patelrudresh <rudreshpatel728@gmail.com>
2025-07-02 10:43:57 +0530

    Configure config server integration and add messageController

--- Employee-service/src/main/java/com/MVC/Employee_service/service/APIClient.java
+++ Employee-service/src/main/java/com/MVC/Employee_service/service/APIClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.MVC.Employee_service.dto.departmentDto;
 
-@FeignClient(name="DEPARTMEN-SERVICE")
+@FeignClient(name="DEPARTMENT-SERVICE")
 public interface APIClient {
 	@GetMapping("api/dept/{deptCode}")
 	departmentDto getbydeparmentId(@PathVariable("deptCode") String deptcode);
